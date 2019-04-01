172e212b9811efabd69348b3a0b8e952d6a46723
104062239 <a0961275586@gmail.com>
2019-04-02 02:14:29 +0800

    Tree ok

--- frequent_pattern.py
+++ frequent_pattern.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import operator
+import copy
 
 arg = sys.argv
 
@@ -19,36 +20,60 @@ def sort_pattern(pat, inv_word_idx):
     ls.sort()
     return ls
 
+def abs_2_trans(abstract , stop_words):
+    abstract = abstract.lower()
+    abstract = re.sub('[\\n]',' ',abstract)
+    abstract = re.sub('[!@#$%^&*()\\n$:;]+','',abstract)
+    sentences = re.split('[.,?]+',abstract)
+    
+    ret = []
+    
+    for se in sentences:
+        k = se.split(' ')
+        a = []
+        for s in k:
+            if s not in stop_words and len(s)>0 and s not in a:
+                a.append(s)
+        if len(a)>0:
+            ret.append(a)
+    return ret
+
 def genTransactionItem():
     in_file = arg[1]
-    inFile = open(in_file, 'r')
+    inFile = open(in_file, 'r').readlines()
     stop = open('stop_words.txt', 'r')
     stopWords = stop.read().split()
 
-    abstractSplitSentence = re.split('[,.?]+', inFile.read().lower())
-
-    transaction = []
-    for splitSentence in abstractSplitSentence:  
-        splitSentence = re.sub('[!@#$%^&*()\\n$:;]', '', splitSentence)
-        transaction.append(splitSentence)
-    abstractSpiltWord = [i.strip().split() for i in transaction]
-
     transactionItem = []
-    for sentence in abstractSpiltWord:
-        reserveItem = []
-        for item in sentence:
-            if item not in stopWords:
-                reserveItem.append(item)
-        transactionItem.append(reserveItem)                       
+    for line in inFile:
+        transactionItem += abs_2_trans(line, stopWords)
 
     return transactionItem
 
+    # abstractSplitSentence = re.split('[,.?]+', inFile.read().lower())
+
+    # transaction = []
+    # for splitSentence in abstractSplitSentence:  
+    #     splitSentence = re.sub('[!@#$%^&*()\\n$:;]', '', splitSentence)
+    #     transaction.append(splitSentence)
+    # abstractSpiltWord = [i.strip().split() for i in transaction]
+
+    # transactionItem = []
+    # for sentence in abstractSpiltWord:
+    #     reserveItem = []
+    #     for item in sentence:
+    #         if item not in stopWords:
+    #         # if item not in stopWords and item not in reserveItem:
+    #             reserveItem.append(item)
+    #     transactionItem.append(reserveItem)
+    # return transactionItem         
+
 def genWordCount(trancsactionItem):
     wordCount = {}
-    for sentence in trancsactionItem:
-        for word in sentence:
+    for trans in trancsactionItem:
+        for word in trans:
             if word not in wordCount:
-                wordCount[word] =  1
+                wordCount[word] = 1
             else:
                 wordCount[word] += 1
     sortedWordCount = sorted(wordCount.items(), key=operator.itemgetter(1), reverse=True)
@@ -56,14 +81,32 @@ def genWordCount(trancsactionItem):
     return sortedWordCount
 
 def buildHeaderTable(wordCount, minSup):
-    headerTable = [tup for tup in wordCount if tup[1] >= minSup]
-    return headerTable
+    headerWord = []
+    # headerTable = {}
+    reverseHeaderTable = {}
+
+    for tup in wordCount: 
+        if tup[1] >= minSup:
+            # headerTable[tup[0]] = tup[1]
+            headerWord.append(tup[0])
+    
+    for tup in reversed(wordCount):
+        if tup[1] >= minSup:
+            reverseHeaderTable[tup[0]] = tup[1]
+    
+    for i in reverseHeaderTable:
+        reverseHeaderTable[i] = [reverseHeaderTable[i], None]
+
+    # for i in headerTable:
+    #     headerTable[i] = [headerTable[i], None]
+
+    return reverseHeaderTable, headerWord
 
 def excludeFreqItem(transactionItem, headerWord):
     freqTable = []
-    for sentence in transactionItem:
+    for trans in transactionItem:
         freqtran = []
-        for item in sentence:
+        for item in trans:
             if item in headerWord:
                 freqtran.append(item)
         if len(freqtran) > 0:
@@ -77,16 +120,104 @@ def sortFreqItem(freqTable, headerWord):
         tranItem = []
         for word in headerWord:
             if word in tid:
-                for w in tid:
-                    if w == word:
-                        tranItem.append(w)
+                tranItem.append(word)
+                # for w in tid:
+                #     if w == word:
+                #         tranItem.append(w)
         sortFreqTable.append(tranItem)
             
     return sortFreqTable
 
-def genHeaderWord(headerTable):
-    headerWord = [word for word, freq in headerTable]
-    return headerWord
+class treeNode:
+    def __init__(self, word, count, parentNode):
+        self.name = word
+        self.count = count
+        self.sumCount = count
+        self.parent = parentNode
+        self.children = {}
+        self.childrenTemp = {}
+        self.next = None
+
+    def add(self):
+        self.count += 1
+
+    def emptyChildren(self):
+        self.childrenTemp = self.children
+        self.children = {}
+
+    def recoverChildren(self):
+        self.children = self.childrenTemp
+        self.childrenTemp = {}
+    
+    def display(self):
+        print (self.name, self.count)
+        for child in self.children.values():
+            print(child)
+            child.display()
+            
+
+def createTree(trans, headerTable, root):
+    if trans[0] in root.children:
+        root.children[trans[0]].add()
+    else:
+        root.children[trans[0]] = treeNode(trans[0], 1, root)
+
+        if headerTable[trans[0]][1] == None:
+            headerTable[trans[0]][1] = root.children[trans[0]]
+        else:
+            updateHeaderTable(headerTable[trans[0]][1], root.children[trans[0]])
+
+    if len(trans) > 1:
+        createTree(trans[1:], headerTable, root.children[trans[0]])
+
+def updateHeaderTable(headerNode, newChildNode):
+    while(headerNode.next != None):
+        headerNode = headerNode.next
+    headerNode.next = newChildNode
+
+def suffixTraverse(rootNode, headerTable, minSup):
+    for item in headerTable:
+        # print('------------------------', item, '--------------------------------------')
+        deleteLeaf(headerTable[item][1])
+        sumChildValue(rootNode, item)
+        pickFpSet(rootNode, item, minSup)
+        recoverLeaf(headerTable[item][1])
+        # break
+        # print(item, rootNode.sumCount)  
+               
+def deleteLeaf(headerNode):
+    p = headerNode
+    while(p != None):
+        p.emptyChildren()
+        p = p.next
+
+def recoverLeaf(headerNode):
+    p = headerNode
+    while(p.next != None):
+        p.recoverChildren()
+        p = p.next
+
+def sumChildValue(rootNode, name):
+    rootNode.sumCount = 0
+    if not bool(rootNode.children):
+        if rootNode.name == name:
+            return rootNode.count
+        else:
+            return 0
+
+    for child in rootNode.children.values():
+        rootNode.sumCount += sumChildValue(child, name)
+
+    return rootNode.sumCount
+
+def pickFpSet(rootNode, name, minSup):
+    if rootNode.name == name:
+        freqSet = []
+        freqSet.append(name)
+        return         
+    for child in rootNode.children.values():
+        pickFpSet(child, minSup)
+
 
 def main():
     out_file, min_sup = arg[2], arg[3]
@@ -94,12 +225,16 @@ def main():
     outFile = open(out_file, 'w')
     transactionItem = genTransactionItem()
     wordCount = genWordCount(transactionItem)
-    headerTable = buildHeaderTable(wordCount, minSup)
-    headerWord = genHeaderWord(headerTable)
+    reverseHeaderTable, headerWord = buildHeaderTable(wordCount, minSup)
     freqTable = excludeFreqItem(transactionItem, headerWord)
     sortFreqTable = sortFreqItem(freqTable, headerWord)
+    # create tree and header table linklist
+    rootNode = treeNode('Root', 0, None)
+    for trans in sortFreqTable:
+        createTree(trans, reverseHeaderTable, rootNode)
 
-    print(sortFreqTable)
+    suffixTraverse(rootNode, reverseHeaderTable, minSup)
+    # print(wordCount)
 
-
+    
 main()
